4a96e365760aa0183006feacd2df5936587d00dd
Eric Petersen <eric@huskers.unl.edu>
2015-01-15 20:46:01 -0600

    Adding tests that demonstrate the ORA-12704 error

--- dummy/tests.py
+++ dummy/tests.py
@@ -1,3 +1,52 @@
+from django.db import DatabaseError
 from django.test import TestCase
 
-# Create your tests here.
+from dummy.models import Dummy
+
+
+class TestDummyBulkCreation(TestCase):
+    def _assertBulkCreateWorks(self, objs, batch_size=None):
+        """
+        Create records with bulk_create, watching for DatabaseErrors.
+
+        If batch_size=None, Django will set batch_size=len(objs).
+        """
+        cls = objs[0].__class__
+        try:
+            cls.objects.bulk_create(objs, batch_size=batch_size)
+        except DatabaseError as e:
+            self.fail(
+                'Bulk creation of {n} {cls} objects with '
+                'batch_size={batch} raised the following '
+                'exception: {exc}'.format(
+                    n=len(objs), cls=cls, batch=batch_size, exc=e,
+                )
+            )
+
+    def test_no_fields_set(self):
+        objs = [Dummy(), Dummy()]
+        self._assertBulkCreateWorks(objs)
+        for n in [1, 2, 3, 4, 10, 20, 50, 100, 1000]:
+            self._assertBulkCreateWorks(objs, batch_size=n)
+
+    def test_same_field_set(self):
+        objs = [Dummy(a=u'123.45'), Dummy(a=44.42)]
+        for n in [1, 2, 3, 4, 10, 20, 50, 100, 1000]:
+            self._assertBulkCreateWorks(objs, batch_size=n)
+
+    def test_different_fields_set(self):
+        objs = [Dummy(a=u'123.45'), Dummy(b=u'94324.35')]
+        self._assertBulkCreateWorks(objs)
+
+    def test_different_fields_set_with_batch_size_1(self):
+        objs = [Dummy(a=u'123.45'), Dummy(b=u'94324.35')]
+        self._assertBulkCreateWorks(objs, batch_size=1)
+
+    def test_different_fields_set_with_batch_size_2(self):
+        objs = [Dummy(a=u'123.45'), Dummy(b=u'94324.35')]
+        self._assertBulkCreateWorks(objs, batch_size=2)
+
+    def test_different_fields_set_with_batch_size_35(self):
+        objs = [Dummy(a=u'123.45'), Dummy(b=u'94324.35')]
+        self._assertBulkCreateWorks(objs, batch_size=35)
+
