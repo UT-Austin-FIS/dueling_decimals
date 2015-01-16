b97d20304432aaeace0cd5a13ea831469a05178a
Eric Petersen <eric@huskers.unl.edu>
2015-01-15 20:26:22 -0600

    Adding django project skeleton

---
+++ dueling_decimals/_local_settings_template.py
@@ -0,0 +1,14 @@
+ALLOWED_HOSTS = ['localhost']
+
+SECRET_KEY = 'xl9@r8d+msu(o5(2skxi-^70p&)a1_tmtnd)g-mkzu0x(q3i-a'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '1521',
+    },
+}
