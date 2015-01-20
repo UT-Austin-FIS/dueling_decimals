2c63c277f01e7572f218aed36b28b5b8c1c698a1
Eric Petersen <eric@huskers.unl.edu>
2015-01-20 10:59:10 -0600

    Adding option to create good records with bulk_create

--- dummy/management/commands/single_bulk_create.py
+++ dummy/management/commands/single_bulk_create.py
@@ -1,9 +1,18 @@
-from django.core.management.base import BaseCommand
+from django.core.management.base import LabelCommand
 
 from dummy.models import Dummy
 
 
-class Command(BaseCommand):
-    def handle(self, **options):
-        objs = [Dummy(a=u'1.00'), Dummy(b=u'2.00')]
+class Command(LabelCommand):
+    help = (
+        'Do a single bulk_create in the Dummy table. '
+        'Use the label "good" for a bulk_create that is expected to work.'
+        'Use any other label for a bulk_create that is expected to fail.'
+    )
+
+    def handle(self, label, **options):
+        if label == 'good':
+            objs = [Dummy(a=u'1.00'), Dummy(a=u'2.00')]
+        else:
+            objs = [Dummy(a=u'1.00'), Dummy(b=u'2.00')]
         Dummy.objects.bulk_create(objs, batch_size=35)
